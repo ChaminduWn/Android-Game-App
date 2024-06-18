44a8574ad5044cc0fd99bbbde2360db4177546f9
ChaminduWN <127916715+chamindu2001@users.noreply.github.com>
2024-06-18 16:04:51 +0530

    add function to main Activity

--- app/src/main/java/com/example/theflyingfish/MainActivity.kt
+++ app/src/main/java/com/example/theflyingfish/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.theflyingfish
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var gameView: GameView
+    private val handler = Handler()
+    private val interval: Long = 30
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        gameView = GameView(this)
+        setContentView(gameView)
+
+        val timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                handler.post {
+                    gameView.invalidate()
+                }
+            }
+        }, 0, interval)
     }
 }
